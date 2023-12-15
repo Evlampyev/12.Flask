f17daad0db12ed5230f3a521965a8a8398902c1c
Alex <Evlampyev.Alex@gmail.com>
2023-12-15 15:02:29 +0500

    home_2 end

--- hometasks/home_2/task_8/task_8.py
+++ hometasks/home_2/task_8/task_8.py
@@ -2,7 +2,7 @@
 # При нажатии на кнопку будет произведено перенаправление на страницу с flash сообщением, где будет
 # выведено "Привет, {имя}!".
 
-from flask import Flask, request, render_template, flash, redirect
+from flask import Flask, request, render_template, flash, redirect, url_for
 
 app = Flask(__name__)
 app.secret_key = 'a4cf4bb73b3ef32a7f8454d20c430f74bc79789692e66faafc2dd118b7e269d6'
@@ -13,11 +13,15 @@ def index():
     return render_template('index.html')
 
 
-@app.post('/flash')
-def flash():
+@app.post('/')
+def index_post():
     name = request.form['user_name']
-    flash(f'Привет, {name}', 'danger')
-    return redirect('index.html')
+    if name:
+        flash(f'Привет, {name}', 'success')
+        return redirect(url_for('index'))
+    else:
+        flash(f'Введите имя', 'danger')
+        return redirect(url_for('index'))
 
 
 if __name__ == '__main__':
